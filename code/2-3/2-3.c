f59421d48039a56e7583fcc9c791a6c2fc8864f4
Kanyon05 <windtree01@gmail.com>
2026-03-27 11:36:29 +0900

    Update 2-3.c

---
+++ code/2-3/2-3.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <string.h>
+
+int main() {
+    char a[64] = "Hello";
+    char b[64] = "world";
+    char c[128];
+
+    printf("length of a: %d\n", sizeof(a));
+
+    strcpy(c, a);
+    printf("a: %s, c: %s\n", a, c);
+
+    strcat(c, " ");
+    strcat(c, b);
+    printf("c: %s\n", c);
+
+    printf("strcmp(%s, %s) = %d\n", a, a, strcmp(a, a));
+    printf("strcmp(%s, %s) = %d\n", a, b, strcmp(a, b));
+
+    return 0;
+}
